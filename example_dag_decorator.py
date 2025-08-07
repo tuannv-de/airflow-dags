dc1e4aa46aa52527470fdecf43351ea06a3827f4
Nguyen Van Tuan <147312701+tuannv-de@users.noreply.github.com>
2025-08-07 14:11:48 +0700

    Update example_dag_decorator.py

--- example_dag_decorator.py
+++ example_dag_decorator.py
@@ -18,48 +18,9 @@ with DAG(
     catchup=False,
     tags=['spark', 'k8s'],
 ) as dag:
-    spark_app = {
-        "apiVersion" : "sparkoperator.k8s.io/v1beta2",
-        "kind" : "SparkApplication",
-        "metadata" : {
-            "name"      : "money-transfer-spark-streaming",
-            "namespace" : "streaming-pipeline",
-        },
-        "spec" : {
-            "type"               : "Scala",
-            "pythonVersion"      : "3",
-            "mode"               : "cluster",
-            "image"              : "spark:3.5.3",
-            "imagePullPolicy"    : "IfNotPresent",
-            "mainClass"          : "org.apache.spark.examples.SparkPi",
-            "mainApplicationFile": "local:///opt/spark/examples/jars/spark-examples.jar",
-            "sparkVersion"       : "3.5.3",
-            "arguments": ["5000"],
-
-            "driver" : {
-                "labels" : {
-                    "version" : "3.5.3"
-                },
-                "serviceAccount" : "spark",
-                "cores"          : 1,
-                "memory"         : "1g",
-                "env" : []
-            },
-            "executor" : {
-                "labels" : {
-                    "version" : "3.5.3"
-                },
-                "instances" : 1,
-                "cores"     : 2,
-                "memory"    : "512m",
-                "env" : []
-            }
-        }
-    }
-
     submit_spark = SparkKubernetesOperator(
         task_id='submit_spark_app',
-        application=spark_app,
+        application_file="sparkapp.yaml",
         kubernetes_conn_id='microk8s_default',  # Airflow connection to your microk8s cluster
         namespace='streaming-pipeline',
         do_xcom_push=True,
